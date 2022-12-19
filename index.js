54b4fae791e7ac2ce8bc44957d23a4fb745afc26
devansh07mehta <devansh1622mht@gmail.com>
2022-12-19 07:06:46 +0530

    New routes created

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+
+//importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
 const app = express();
 const PORT = 8081;
-app.use(express.json());
+app.use(express.json()); // To transfer the data(i.e response from the server) in the JSON format.
 
 // command for saving nodemon as developer dependency with short abbreviation named dev => npm i nodemon --save-dev
+// const data = ["devansh","rohan"];
+
+app.get("/", (req,res) => {
+    res.status(200).json({
+        message: "Server is up and running successfully",
+    });
+});
+
+app.use("/users", usersRouter); // http://localhost:8081/users
+app.use("/books", booksRouter); // http://localhost:8081/books
